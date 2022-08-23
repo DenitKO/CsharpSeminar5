ecdd53d9d8e03d7c265504c9b6eeeee2c8f4c2b8
DenitKO <motovdenis@mail.ru>
2022-08-23 23:36:21 +0300

    added a third programm

---
+++ HW3/Program.cs
@@ -0,0 +1,67 @@
+﻿void FillArray (int [] collection)
+{
+    int LengthOfArr = collection.Length;
+    int i = 0;
+    while (i < LengthOfArr)
+    {
+        collection[i] = new Random().Next(1,100);
+        i++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]+" ");
+        position++;
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество ячеек массива ");
+
+int amount = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[amount];
+
+FillArray(array);
+
+PrintArray(array);
+
+int MaxCountArray (int [] SomeMaxArray)
+{
+    int Count = SomeMaxArray.Length;
+    int index = 1;
+    int MaxCount = SomeMaxArray[0];
+    while (index < Count)
+    {
+        if (MaxCount < SomeMaxArray[index])
+            MaxCount = SomeMaxArray[index];
+        index++;
+    }
+    return MaxCount;
+}
+
+int MinCountArray (int [] SomeMinArray)
+{
+    int Count = SomeMinArray.Length;
+    int MinIndex = 1;
+    int MinCount = SomeMinArray[0];
+    while (MinIndex < Count)
+    {
+        if (MinCount > SomeMinArray[MinIndex])
+            MinCount = SomeMinArray[MinIndex];
+        MinIndex++;
+    }
+    return MinCount;
+}
+
+int MaxDigit = MaxCountArray(array);
+
+int MinDigit = MinCountArray(array);
+
+Console.Write("Разница между максимальным и минимальным эллементом массива = ");
+Console.WriteLine(MaxDigit-MinDigit);
